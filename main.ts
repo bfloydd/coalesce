64171e38795296160a956127e74bf8a0674f8d36
Ben Floyd <ben.floyd@simplot.com>
2024-11-05 15:25:57 -0700

    format

--- main.ts
+++ main.ts
@@ -18,13 +18,16 @@ export default class CoalescePlugin extends Plugin {
 			}
 		});
 
-		// Usage: In the browser console, type `enableLogging()` to enable logging.
+		/**
+		 * Logging
+		 * Usage: In the browser console, type `enableLogging()` to enable logging.
+		 * Usage: In the browser console, type `disableLogging()` to disable logging.
+		 */
+		// 
 		(window as any).enableLogging = function() {
 			Logger.enable();
 			console.log("Logging enabled");
 		};
-
-		// Usage: In the browser console, type `disableLogging()` to disable logging.
 		(window as any).disableLogging = function() {
 			Logger.disable();
 			console.log("Logging disabled");
