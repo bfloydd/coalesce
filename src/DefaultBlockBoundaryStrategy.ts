087d4c3a9700090f0f42447ecacdf840f5938454
Ben Floyd <ben.floyd@simplot.com>
2024-11-08 22:35:03 -0700

    Fix matching to include full path

--- src/DefaultBlockBoundaryStrategy.ts
+++ src/DefaultBlockBoundaryStrategy.ts
@@ -3,10 +3,21 @@ import { BlockBoundaryStrategy } from './BlockBoundaryStrategy';
 export class DefaultBlockBoundaryStrategy implements BlockBoundaryStrategy {
     findBlockBoundaries(content: string, currentNoteName: string): { start: number, end: number }[] {
         const boundaries: { start: number, end: number }[] = [];
-        const regex = new RegExp(`\\[\\[${currentNoteName}\\]\\]`, 'g');
-        let match;
+        const escapedNoteName = currentNoteName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        const regex = new RegExp(`\\[\\[([^\\]]*\\/)*${escapedNoteName}(\\|[^\\]]*)?\\]\\]`, 'g');
+        
+        /*
+        [[currentNoteName]]
+        [[Full/Path/currentNoteName]]
+        [[Full/Path/currentNoteName|shortname]] 
+        console.log('Escaped note name:', escapedNoteName);
+        console.log('Regex pattern:', regex.source);
+        */
 
+
+        let match;
         while ((match = regex.exec(content)) !== null) {
+            console.log('Found match:', match[0]);
             const lineStartIndex = content.lastIndexOf('\n', match.index) + 1;
             const endIndex = content.indexOf('---', match.index);
             const nextMentionIndex = content.indexOf(`[[${currentNoteName}]]`, match.index + 1);
