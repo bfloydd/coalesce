6cb17d5849ca6e2b5a08c8250acb7ba7f3f3adaf
Ben Floyd <ben.floyd@simplot.com>
2025-01-17 10:12:34 -0700

    Add thememanager

---
+++ src/utils/ThemeManager.ts
@@ -0,0 +1,15 @@
+export class ThemeManager {
+    private static readonly THEMES = ['default', 'minimal', 'modern', 'naked'] as const;
+    
+    public static get themes(): readonly string[] {
+        return this.THEMES;
+    }
+
+    public static isValidTheme(theme: string): boolean {
+        return this.THEMES.includes(theme as any);
+    }
+
+    public static getDefaultTheme(): string {
+        return this.THEMES[0];
+    }
+} 
